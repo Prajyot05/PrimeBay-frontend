9bc3e391ce12ac4de2716432c56f087d2823ff52
Prajyot Tayde <pmtbmt@gmail.com>
2024-11-05 12:25:12 +0530

    fix typescript errors

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,5 +1,4 @@
 import { FaExpandAlt, FaPlus } from "react-icons/fa";
-import { server } from "../redux/store";
 import { CartItem } from "../types/types";
 import { Link } from "react-router-dom";
 import { transformImage } from "../utils/features";
