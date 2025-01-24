31532842c010cd78e6a19ff2ec738b797034902a
Prajyot Tayde <pmtbmt@gmail.com>
2025-01-24 09:12:26 +0530

    fix ts errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense, useEffect } from "react";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
